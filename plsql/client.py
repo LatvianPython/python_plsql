c4da74e1579dc7291f7113098c21cf453e903928
LatvianProgrammer <kalvans.rolands@gmail.com>
2019-09-28 14:21:08 +0300

    experimenting with adding more dotted access

--- plsql/client.py
+++ plsql/client.py
@@ -1,4 +1,37 @@
 import cx_Oracle as oracle
+from functools import lru_cache
+
+
+class Schema:
+    sql_query = '''
+SELECT COUNT(*)
+  FROM all_users
+ WHERE username = :name
+'''
+
+    def __init__(self, name, plsql):
+        self.name = name
+        self.plsql = plsql
+        result = list(plsql.query(sql_query=self.sql_query,
+                                  bind_variables={'name': name.upper()}))
+        if sum(row[0] for row in result) == 0:
+            raise ValueError('Not a database schema!')
+
+
+@lru_cache(maxsize=None)
+def get_schema(name, plsql):
+    try:
+        return Schema(name=name, plsql=plsql)
+    except ValueError:
+        return None
+
+
+class Package:
+    pass
+
+
+class Subprogram:
+    pass
 
 
 class Database:
@@ -11,7 +44,18 @@ class Database:
         with self.connection.cursor() as cursor:
             cursor.execute(dynamic_string)
 
+    def query(self, sql_query, bind_variables):
+        with self.connection.cursor() as cursor:
+            cursor.execute(sql_query, bind_variables)
+            for row in cursor:
+                yield row
+
     def __getattr__(self, item):
+        schema = get_schema(item, self)
+
+        if schema:
+            return schema
+
         def func(**kwargs):
             with self.connection.cursor() as cursor:
                 return cursor.callfunc(item, int, keywordParameters=kwargs)
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,3 +1,6 @@
+from plsql.client import Schema
+
+
 def test_simple_function(plsql, simple_function):
     assert plsql.simple_function(p_int=5) == 5 + 5
     assert plsql.simple_function(p_int=None) is None
@@ -8,3 +11,7 @@ def test_function_with_defaults(plsql, function_with_defaults):
     assert plsql.function_with_defaults(p_int_a=0) == 4
     assert plsql.function_with_defaults(p_int_b=0) == 1
     assert plsql.function_with_defaults(p_int_a=0, p_int_b=0) == 0
+
+
+def test_schema(plsql):
+    assert isinstance(plsql.test_user, Schema)
