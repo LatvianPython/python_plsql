e2c2527dc06b9e3ff7ececb7621d141cf363d5a7
LatvianProgrammer <kalvans.rolands@gmail.com>
2019-09-29 14:36:26 +0300

    naming changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,16 +23,16 @@ def plsql():
         for file in directory.glob('*.sql')
     ]
 
-    create_object(database, objects)
+    create_objects(database, objects)
 
     yield database
 
     drop_all_objects(database, objects)
 
 
-def create_object(plsql, objects):
-    for file, _ in objects:
-        with open(file, mode='r', encoding='utf8') as file:
+def create_objects(plsql, objects):
+    for file_path, _ in objects:
+        with open(file_path, mode='r', encoding='utf8') as file:
             sql_text = file.read()
         plsql.execute_immediate(sql_text)
 
