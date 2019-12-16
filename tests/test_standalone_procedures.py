d877d3c56953c25924b1e79cad325df41d4b7ed9
LatvianProgrammer <kalvans.rolands@gmail.com>
2019-12-16 23:42:03 +0200

    expanding tests a little

--- tests/test_standalone_procedures.py
+++ tests/test_standalone_procedures.py
@@ -1,6 +1,24 @@
 import pytest
 
 
-def test_procedure_found(plsql):
+def test_procedure_found_in_same_schema(plsql):
     procedure = plsql.simple_procedure
     assert procedure.resolved.name == "SIMPLE_PROCEDURE"
+
+
+def test_procedure_found_with_synonym(plsql):
+    procedure = plsql.test_synonym_procedure
+    assert procedure.resolved.name == "SIMPLE_PROCEDURE"
+
+
+@pytest.fixture(scope="module", params=["simple_procedure", "test_synonym_procedure"])
+def procedure(plsql, request):
+    return getattr(plsql, request.param)
+
+
+def test_standalone_procedures_are_not_overloaded(plsql, procedure):
+    assert not procedure.overloaded
+
+
+def test_standalone_procedures_are_standalone(plsql, procedure):
+    assert procedure.standalone
